570dc3129b5314190bf1911f36017fc28757ef7b
nickolanico900-a11y <nickolanico900@gmail.com>
2025-10-30 17:30:12 +0200

    Updated StickerPacks.tsx

--- src/components/StickerPacks.tsx
+++ src/components/StickerPacks.tsx
@@ -78,6 +78,7 @@ const packs: StickerPack[] = [
     stickers: 1,
     price: 99,
     bonus: 0,
+    productToCount,
     carImage: 'https://i.ibb.co/nNw9f5vz/base6.png',
     isPhysical: true,
     description: '6х7 см наліпка'
