1b1a09446ec3237f2e9931a95e1af03b4d35d7c6
nickolanico900-a11y <nickolanico900@gmail.com>
2025-10-31 23:37:24 +0200

    Added 20251031213609_fix_reset_position_sequence_for_deleted_entries.sql

---
+++ supabase/migrations/20251031213609_fix_reset_position_sequence_for_deleted_entries.sql
@@ -0,0 +1,16 @@
+/*
+  # Fix reset_position_sequence to work after deletion
+
+  1. Changes
+    - Update reset_position_sequence to only reset the sequence counter
+    - Remove the UPDATE statement that sets position_number to NULL
+    - This function is called after DELETE, so there are no records to update
+    - Next insert will correctly start from position 1
+*/
+
+CREATE OR REPLACE FUNCTION reset_position_sequence()
+RETURNS void AS $$
+BEGIN
+  ALTER SEQUENCE sticker_position_seq RESTART WITH 1;
+END;
+$$ LANGUAGE plpgsql;
