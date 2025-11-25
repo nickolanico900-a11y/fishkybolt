717f05128cd6c1536a6eee29db86d98d44650e7e
nickolanico900-a11y <nickolanico900@gmail.com>
2025-11-25 20:13:57 +0200

    Updated index.ts

--- supabase/functions/send-confirmation-email/index.ts
+++ supabase/functions/send-confirmation-email/index.ts
@@ -106,7 +106,19 @@ Deno.serve(async (req: Request) => {
    const emailBody = `
   <!DOCTYPE html>
   <html>
-    <head>
+    <head><!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2005117223585958');
+fbq('track', 'PageView');
+</script>
       <meta charset="utf-8">
       <style>
         body { font-family: Arial, sans-serif; line-height: 1.6; color: #333; background: #eee; }
@@ -120,6 +132,10 @@ Deno.serve(async (req: Request) => {
       </style>
     </head>
     <body>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=2005117223585958&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
       <div class="container">
         <div class="header">
           <h1>Дякуємо за замовлення!</h1>
